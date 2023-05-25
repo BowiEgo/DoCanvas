6e73e366dc547aea660aca75ff445432605dc15f
bowiego <295030485@qq.com>
2023-05-25 21:05:18 +0800

    chore: some update

--- site/src/App.vue
+++ site/src/App.vue
@@ -3,7 +3,7 @@ import { ref } from 'vue'
 import HtmlToCanvas from './components/htmlToCanvas.vue'
 
 const rawHtml = ref(
-  '<div style="height: 422px; border-bottom: 1px solid rgb(0, 0, 0); box-sizing: border-box;"><div style="color: red; background-color: rgba(0, 174, 236, 0.27); width: 300px; height: auto; margin-top: 40px;"><span>😱一、选择适合的报纸在进行登报挂失前，您需要选择一家有一定知名度和影响力的报纸进行登报。目前，大多数地区都有本地的日报、晚报、周报等媒体，您可以选择其中的一家来登报。二、撰写登报文章登报文章的撰写需要遵守一定的规定和格式。一般来说，登报文章应该包含以下几个方面的内容： 👌</span><span></span></div><div style="background-color: green; width: 50px; height: 40px;"></div><div style="display: inline-block; background-color: lightgreen; width: 150px; height: 50px;"></div><div style="display: inline-block; background-color: darkorange; width: 150px; height: 50px;"></div><div style="display: inline-block; background-color: pink; width: 150px; height: 50px;"></div></div>'
+  '<div style="height: 422px; border-bottom: 1px solid rgb(0, 0, 0); box-sizing: border-box;"><div style="color: red; background-color: rgba(0, 174, 236, 0.27); width: 300px; height: auto; margin-top: 40px;"><span>一、选择适合的报纸在进行登报挂失前，您需要选择一家有一定知名度和影响力的报纸进行登报。目前，大多数地区都有本地的日报、晚报、周报等媒体，您可以选择其中的一家来登报。二、撰写登报文章登报文章的撰写需要遵守一定的规定和格式。一般来说，登报文章应该包含以下几个方面的内容： 👌</span><span></span></div><div style="background-color: green; width: 50px; height: 40px;"></div><div style="display: inline-block; background-color: lightgreen; width: 150px; height: 50px;"></div><div style="display: inline-block; background-color: darkorange; width: 150px; height: 50px;"></div><div style="display: inline-block; background-color: pink; width: 150px; height: 50px;"></div></div>'
 )
 </script>
 
